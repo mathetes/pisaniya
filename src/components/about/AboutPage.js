f82042656087cae878b01964dce5c978b7ff552d
mathetes <nsk.roman@gmail.com>
2017-05-01 23:56:13 +0700

    clean template

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>О приложении</h1>
+				<p>Приложение нацелено на представление Библейских фактов в удобной, интерактивной форме.</p>
+			</div>
+		);
+	}
+}
+
+export default AboutPage;
