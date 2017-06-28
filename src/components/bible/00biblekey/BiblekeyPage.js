3586d1f2e4708a167b226563a96df392595eb1f2
mathetes <nsk.roman@gmail.com>
2017-06-28 16:32:56 +0700

    styled table

--- src/components/bible/00biblekey/BiblekeyPage.js
+++ src/components/bible/00biblekey/BiblekeyPage.js
@@ -15,27 +15,19 @@ const styleConfig = {
   styles: {
     Table: { margin: "0 auto", textAlign:"left", borderBottom: "1px solid hsla(0,0%,0%,0.12)", paddingTop: "0.75rem", paddingBottom: "calc(0.75rem - 1px)"},
   	TableHeading: { display: "table-header-group", verticalAlign: "middle", borderColor: "inherit", backgroundColor: "#222", color: "#fff", textTransform: "uppercase" },
-  	TableHeadingCell: { textAlign: "center", borderBottom: "1px solid hsla(0,0%,0%,0.12)", fontFeatureSettings: "tnum",
-paddingLeft: "1rem", paddingRight: "1rem", paddingTop: "0.75rem", paddingBottom: "calc(0.75rem - 1px)" },
+  	TableHeadingCell: { textAlign: "center", borderBottom: "1px solid hsla(0,0%,0%,0.12)", fontFeatureSettings: "tnum",paddingLeft: "1rem", paddingRight: "1rem", paddingTop: "0.75rem", paddingBottom: "calc(0.75rem - 1px)" },
   	Row: { backgroundColor: "#eee", lineHeight: "2", padingLeft: "15px"},
-  	Cell: { textAlign:" left", borderBottom: "1px solid hsla(0,0%,0%,0.12)", fontFeatureSettings: "tnum", paddingLeft: "1rem",
-paddingRight: "1rem", paddingTop: "0.75rem", paddingBottom: "calc(0.75rem - 1px)" },
-  	Filter: { display: "block", margin: "2em auto", padding: "0.5em 0.25em", border: "none", borderRadius: "0.2em",
-fontSize: "1.5em", textAlign: "center", boxShadow: "0 0 1em 0.25em rgba(0,0,0,0.2)", width: "50%" },
-	PageDropdown: { display: "inline-block", fontSize: "18px", margin: "0 5px", padding: "0 5px", color: "#649",
-background: "transparent", border: "1px solid #649", height: "32px" },
-	NextButton: { display: "inline-block", fontSize: "18px", margin: "0 5px", padding: "0 5px", color: "#649",
-background: "transparent", border: "1px solid #649", height: "32px" },
-	PreviousButton: { display: "inline-block", fontSize: "18px", margin: "0 5px", padding: "0 5px", color: "#649",
-background: "transparent", border: "1px solid #649", height: "32px", content: "Назад"},
-	SettingsToggle: { display: "inline-block", fontSize: "18px", margin: "0 5px", padding: "0 5px", color: "#649",
-background: "transparent", border: "1px solid #649", height: "32px" },
-	Settings: { display: "inline-block", fontSize: "18px", margin: "0 5px", padding: "0 5px", color: "#649",
-background: "transparent", border: "1px solid #649", height: "32px" }
+  	Cell: { textAlign:" left", borderBottom: "1px solid hsla(0,0%,0%,0.12)", fontFeatureSettings: "tnum", paddingLeft: "1rem",paddingRight: "1rem", paddingTop: "0.75rem", paddingBottom: "calc(0.75rem - 1px)" },
+  	Filter: { display: "block", margin: "2em auto", padding: "0.5em 0.25em", border: "none", borderRadius: "0.2em",fontSize: "1.5em", textAlign: "center", boxShadow: "0 0 1em 0.25em rgba(0,0,0,0.2)", width: "50%" },
+	PageDropdown: { display: "inline-block", fontSize: "18px", margin: "0 5px", padding: "0 5px", color: "#649",background: "transparent", border: "1px solid #649", height: "32px" },
+	NextButton: { display: "inline-block", fontSize: "18px", margin: "0 5px", padding: "0 5px", color: "#649",background: "transparent", border: "1px solid #649", height: "32px" },
+	PreviousButton: { display: "inline-block", fontSize: "18px", margin: "0 5px", padding: "0 5px", color: "#649",background: "transparent", border: "1px solid #649", height: "32px", content: "Назад"},
+	SettingsToggle: { display: "inline-block", fontSize: "18px", margin: "0 5px", padding: "0 5px", color: "#649",background: "transparent", border: "1px solid #649", height: "32px" },
+	Settings: { display: "inline-block", fontSize: "18px", margin: "0 5px", padding: "0 5px", color: "#649",background: "transparent", border: "1px solid #649", height: "32px" }
   }
 }
 
-
+const CustomColumn = ({value}) => <span style={{ color: '#0000AA' }}>{value}</span>;
 
 class BiblekeyPage extends React.Component {
     render() {
@@ -43,32 +35,32 @@ class BiblekeyPage extends React.Component {
             <div>
                 <h1>Ключевые параметры книг Библии</h1>
                 <Griddle data={data} plugins={[plugins.LocalPlugin]} styleConfig={styleConfig} pageProperties={{currentPage: 1,
-          	pageSize: 66,
-          	recordCount: 100}}
-          events={{onFilter: (filterText) => { },
-          onSort: (sortProperties) => {},
-          onNext: () => {},
-          onPrevious: () => {},
-          onGetPage: (pageNumber) => {},
-  }}>
-    <RowDefinition>
-        <ColumnDefinition id="id" title="Номер" />
-        <ColumnDefinition id="bookName" title="Название" />
-        <ColumnDefinition id="bookKeyVerse" title="Кл.стих" />
-        <ColumnDefinition id="bookSubject" title="Предмет" />
-        <ColumnDefinition id="bookKeyWord" title="Кл.слово" />
-        <ColumnDefinition id="bookMainTheme" title="Тема" />
-        <ColumnDefinition id="BookDateWriting" title="Дата написания" />
-        <ColumnDefinition id="bookAuthor" title="Автор" />
-        <ColumnDefinition id="bookRecipient" title="Получатели" />
-        <ColumnDefinition id="bookPlaceWriting" title="Место написания" />
-        <ColumnDefinition id="bookChapters" title="Глав" />
-        <ColumnDefinition id="bookVerses" title="Стихов" />
-        <ColumnDefinition id="bookAllWords" title="Слов" />
-        <ColumnDefinition id="bookCovenant" title="Завет" />
-        <ColumnDefinition id="bookGanre" title="Жанр" />
-			</RowDefinition>
-          </Griddle>
+                        pageSize: 66,
+                        recordCount: 100}}
+                      events={{onFilter: (filterText) => { },
+                      onSort: (sortProperties) => {},
+                      onNext: () => {},
+                      onPrevious: () => {},
+                      onGetPage: (pageNumber) => {},
+              }}>
+                <RowDefinition>
+                    <ColumnDefinition id="id" title="Номер" order={1} />
+                    <ColumnDefinition id="bookName" title="Название" order={2}  customComponent={CustomColumn} />
+                    <ColumnDefinition id="bookKeyVerse" title="Кл.стих" order={3} />
+                    <ColumnDefinition id="bookSubject" title="Предмет" order={4} />
+                    <ColumnDefinition id="bookKeyWord" title="Кл.слово" order={5} />
+                    <ColumnDefinition id="bookMainTheme" title="Тема" order={6} />
+                    <ColumnDefinition id="BookDateWriting" title="Дата написания" />
+                    <ColumnDefinition id="bookAuthor" title="Автор" />
+                    <ColumnDefinition id="bookRecipient" title="Получатели" />
+                    <ColumnDefinition id="bookPlaceWriting" title="Место написания" />
+                    <ColumnDefinition id="bookChapters" title="Глав" />
+                    <ColumnDefinition id="bookVerses" title="Стихов" />
+                    <ColumnDefinition id="bookAllWords" title="Слов" />
+                    <ColumnDefinition id="bookCovenant" title="Завет" />
+                    <ColumnDefinition id="bookGanre" title="Жанр" />
+                </RowDefinition>
+              </Griddle>
             </div>
         );
     }
